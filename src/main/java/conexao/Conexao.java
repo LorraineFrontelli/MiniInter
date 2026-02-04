95e51c5837640edb1d083ce1ea7ee47a0851ef25
Lorraine Frontelli <lorrainefrontelli@gmail.com>
2026-02-04 15:16:27 -0300

    Criando a classe conexão, as models e as daos

---
+++ src/main/java/conexao/Conexao.java
@@ -0,0 +1,40 @@
+package conexao;
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.sql.*;
+
+public class Conexao {
+    Connection conn;
+
+    // Variaveis de ambiente para conectar com o BD
+    Dotenv dotenv=Dotenv.load();
+    String DB_URL = dotenv.get("DB_URL");
+    String DB_USUARIO = dotenv.get("DB_USUARIO");
+    String DB_SENHA = dotenv.get("DB_SENHA");
+
+
+    // Método para conectar o BD
+    public Connection conectar(){
+        try {
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection(DB_URL, DB_USUARIO, DB_SENHA);
+        } catch (ClassNotFoundException | SQLException sqleCnfe){
+            sqleCnfe.printStackTrace();
+        }
+        return conn;
+    }
+
+
+    // Método para desconectar o BD
+    public boolean desconectar(Connection conn){
+        try {
+            if (conn != null && !conn.isClosed()) {
+                conn.close();
+                return true;
+            }
+        } catch (SQLException sqle){
+            sqle.printStackTrace();
+        }
+        return false;
+    }
+}
---
+++ src/main/java/dao/TurmaDAO.java
@@ -0,0 +1,4 @@
+package dao;
+
+public class TurmaDAO {
+}
