9826749d859cbbede6802e61293c2097bfe1ce95
ArthurMachado2501 <arthur.machado2501@gmail.com>
2026-02-10 09:22:00 -0300

    Corrigindo a DAO de mensagem

--- src/main/java/dao/MensagemDAO.java
+++ src/main/java/dao/MensagemDAO.java
@@ -1,11 +1,14 @@
 package dao;
 
-import conexao.Conexao;
-import model.Mensagem;
-
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
+import conexao.Conexao;
+import model.Mensagem;
 
 public class MensagemDAO {
 
@@ -21,7 +24,8 @@ public class MensagemDAO {
             pst.setInt(1, mensagem.getIdProfessor());
             pst.setInt(2, mensagem.getIdAluno());
             pst.setString(3, mensagem.getMensagem());
-            pst.setTimestamp(4, Timestamp.valueOf(mensagem.getMensagem()));
+            pst.setTimestamp(4, mensagem.getDataMensagem());
+
 
             ResultSet rs = pst.executeQuery();
             if (rs.next()) {
@@ -37,56 +41,27 @@ public class MensagemDAO {
         return idGerado; // Retorna o ID gerado ou -1 se falhar
     }
 
-    // READ - BUSCAR MENSAGEM PELO ID
-    public Mensagem buscarPorId(int id) {
+    // READ - LISTAR TODO O HISTORICO DE CHAT ENTRE PROFESSOR E ALUNO
+    public List<Mensagem> listarHistoricoAlunoProfessor(int idAluno, int idProfessor) {
         Conexao conexao = new Conexao();
         Connection con = conexao.conectar();
-        Mensagem mensagem = null;
-        String sql = "SELECT * FROM mensagem WHERE id = ?";
-
-        try {
-            PreparedStatement pst = con.prepareStatement(sql);
-            pst.setInt(1, id);
-            ResultSet rs = pst.executeQuery();
-
-            if (rs.next()) {
-                mensagem = new Mensagem(
-                        rs.getInt("id"),
-                        rs.getInt("id_professor"),
-                        rs.getInt("id_aluno"),
-                        rs.getString("mensagem"),
-                        rs.getTimestamp("dt_mensagem")
-                );
-            }
-        } catch (SQLException sqle) {
-            sqle.printStackTrace();
-        } finally {
-            conexao.desconectar(con);
-        }
-
-        return mensagem; // retorna null se não encontrar
-    }
-
-    // READ - BUSCAR MENSAGEM PELO ID DO PROFESSOR
-    public Mensagem buscarPorIdProfessor(int idProfessor) {
-        Conexao conexao = new Conexao();
-        Connection con = conexao.conectar();
-        Mensagem mensagem = null;
-        String sql = "SELECT * FROM mensagem WHERE id_professor = ?";
+        List<Mensagem> mensagens = new ArrayList<>();
+        String sql = "SELECT * FROM mensagem WHERE id_professor = ? AND id_aluno = ? ORDER BY dt_mensagem";
 
         try {
             PreparedStatement pst = con.prepareStatement(sql);
             pst.setInt(1, idProfessor);
+            pst.setInt(2, idAluno);
             ResultSet rs = pst.executeQuery();
 
-            if (rs.next()) {
-                mensagem = new Mensagem(
+            while (rs.next()) {
+                mensagens.add(new Mensagem(
                         rs.getInt("id"),
                         rs.getInt("id_professor"),
                         rs.getInt("id_aluno"),
                         rs.getString("mensagem"),
                         rs.getTimestamp("dt_mensagem")
-                );
+                ));
             }
         } catch (SQLException sqle) {
             sqle.printStackTrace();
@@ -94,38 +69,9 @@ public class MensagemDAO {
             conexao.desconectar(con);
         }
 
-        return mensagem; // retorna null se não encontrar
+        return mensagens;
     }
 
-    // READ - BUSCAR MENSAGEM PELO ID DO ALUNO
-    public Mensagem buscarPorIdAluno(int idAluno) {
-        Conexao conexao = new Conexao();
-        Connection con = conexao.conectar();
-        Mensagem mensagem = null;
-        String sql = "SELECT * FROM mensagem WHERE id_aluno = ?";
-
-        try {
-            PreparedStatement pst = con.prepareStatement(sql);
-            pst.setInt(1, idAluno);
-            ResultSet rs = pst.executeQuery();
-
-            if (rs.next()) {
-                mensagem = new Mensagem(
-                        rs.getInt("id"),
-                        rs.getInt("id_professor"),
-                        rs.getInt("id_aluno"),
-                        rs.getString("mensagem"),
-                        rs.getTimestamp("dt_mensagem")
-                );
-            }
-        } catch (SQLException sqle) {
-            sqle.printStackTrace();
-        } finally {
-            conexao.desconectar(con);
-        }
-
-        return mensagem; // retorna null se não encontrar
-    }
 
     // READ - LISTAR TODAS AS MENSAGENS
     public List<Mensagem> listar() {
@@ -166,7 +112,7 @@ public class MensagemDAO {
         try {
             PreparedStatement pst = con.prepareStatement(sql);
             pst.setString(1, mensagem.getMensagem());
-            pst.setTimestamp(2, Timestamp.valueOf(mensagem.getMensagem()));
+            pst.setTimestamp(2, mensagem.getDataMensagem());
             pst.setInt(3, mensagem.getId());
 
             retorno = pst.executeUpdate();
